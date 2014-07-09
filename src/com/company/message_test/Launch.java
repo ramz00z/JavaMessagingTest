b17782ea2891a07357164f2270ede1a9b3599eb7
NABIL GARDON <luckynab139@hotmail.com>
2014-07-09 13:43:50 +0200

    Initial commit

---
+++ src/com/company/message_test/Launch.java
@@ -0,0 +1,9 @@
+package com.company.message_test;
+
+public class Launch {
+
+    public static void launch(String[] args) {
+	// write your code here
+        System.out.println("Hello My Biaaaatch !");
+    }
+}
